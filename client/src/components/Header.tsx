ec4e8ee593c364f1ef6de33d4d2f662a7fb3250d
nursh <nsheikhmohammed@yahoo.com>
2019-02-25 21:53:12 -0500

    Added header with styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 
 export const Header = () => {
   return (
-    <div>
-      Header
+    <div className="header">
+      <div>
+        <h3>Cauldrom</h3>
+      </div>
+      <nav className="header__nav">
+        <ul className="header__nav__list">
+          <li className="header__nav__item">Welcome, Username</li>
+          <li className="header__nav__item">
+            <NavLink to="/signin" className="header__nav__link">Logout</NavLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
